0c5c4ab1aa86227f5fd39e19f91ede47cee79f22
Bill Dami <billdami@gmail.com>
2019-03-26 15:57:13 -0400

    disable clear completed button if theres no completed todos

--- app/pods/components/todo-footer/component.js
+++ app/pods/components/todo-footer/component.js
@@ -1,4 +1,10 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
 
 export default class TodoFooterComponent extends Component {
+
+  @computed('args.numCompleted')
+  get noCompletedTodos() {
+    return this.args.numCompleted === 0;
+  }
 }
